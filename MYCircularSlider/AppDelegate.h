48e5f48fade9cf235692e3dfaec22b2cc7530ca7
Sehaj Chawla <Sehaj@Sehaj-Chawlas-iMac-2.local>
2012-04-24 22:07:26 -0400

    Initial Commit

---
+++ MYCircularSlider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MYCircularSlider
+//
+//  Created by Sehaj Chawla on 12-04-24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MYCircularSlider/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MYCircularSlider
+//
+//  Created by Sehaj Chawla on 12-04-24.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
